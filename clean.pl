cc67dedab51ab6c0f38745432d64d4c865a1adf7
sujilnt <sujilnt@gmail.com>
2019-02-20 16:16:04 +0000

    intial commit , need to tested in linux

---
+++ clean.pl
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+
+my($path,$regexExpression) = @ARGV;
+my $fileName = "data.txt";
+
+
+if(not defined $path){
+	die "File directory not given, please  try again \n"
+}
+
+print "added file ";
+
+if (not defined $regexExpression) {
+  # if regular expression not added or with the script it automatically consider regular expression as *		
+  $regexExpression="*";
+  print "--Taking default Regular Expression. \n"
+}
+
+if (defined $regexExpression) {
+  print "The regular Expression : $regexExpression \n";
+  my $directorypathx= `pwd`;
+  print `find '$path' -name '$regexExpression' -printf " %kKb %p\n" | sort -h -r > $fileName `;
+
+ }
