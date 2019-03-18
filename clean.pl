71706bb624191d49bffb33ace87354d5948fc1c8
Sujil Anto <sujilnt@gmail.com>
2019-03-18 23:07:47 +0000

    read file working state good

--- clean.pl
+++ clean.pl
@@ -1,9 +1,10 @@
 use strict;
 use warnings;
 use 5.18.0;
+use HTML::TokeParser;
 
 my $directory = $ARGV[0];
-
+my $indexFIle = "www/index.html";
 
 # customReadDirectory {name of the directory} => or return the list of the files . 
 sub customReadDirectory{
@@ -35,12 +36,31 @@ if(not defined $directory){
    # only directory  
    my @subdiretory = grep{$_ !~ m/\.[a-zA-Z]/ } @filesndDir;
    say(@subdiretory);
-   makeDir("test");
+
+   #makeDir("t2.txt");
+  # readFile("www/craters1.html"); # works fine src and href 
+  readFile("www/index.html");
+
 }
 
 
 # creation of directory 
 sub makeDir{
-  my ($argv)= @_; # taking input #test 
+  my ($argv)= @_; # taking   input #test 
    return `mkdir $argv`;
+}
+
+# readFile function that reads and returns src and href in an array
+sub readFile{
+  my $somefile = @_[0];
+  say("ssssssssssss $somefile");
+  my @links = ();
+  my $p = HTML::TokeParser->new($somefile) || die "Can't open: $!";
+  # configure its behaviour
+  while (my $token = $p->get_tag("img","a")) {
+     my $currentlink = $token->[1]{href} || $token->[1]{src};
+     say($currentlink);
+     push @links,$currentlink;
+  }
+  return @links;
 }
