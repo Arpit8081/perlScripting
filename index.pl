c79bbbe9c2c2368acfb8907783dd73da11cc5f96
Sujil Anto <sujilnt@gmail.com>
2019-03-18 01:11:57 +0000

    adding some rubbish code

--- index.pl
+++ index.pl
@@ -2,8 +2,7 @@
     use strict;
     use warnings;
     use 5.18.0;
-
-    my $directory = 'www';
+    my $directory = scalar $ARGV[0];
     my @myNames = ();
     opendir (DIR, $directory) or die $!;
 
@@ -13,21 +12,23 @@
     	     say("DIR ++++++++  $directory"); 
     	     say("this is a  $file");
 
-    	     if($file =~ /html/){
+    	     if($file =~ /\.[a-zA-Z]/){
     	     	print "inside loop \n";
     	     	push @myNames,$file
-    	     }
-   
-    		 
-       
+    	     }		 
     }
 
-    closedir(DIR);
-
-   
+   closedir(DIR);
+my @index = grep {$_ =~ /index/} @myNames ;
+    say("\n");
+    say("\n");
+    say(@myNames);
+    say("\n");
+    say("the is index is @index");
 
 my %hash = (one => "uno",two=> "dos",dir => [("3","4")]);
 
 while (my($k,$v)=each %hash){
 	say "$k  ->  $v";
 }
+
